82eda2ead317198c04b8eca3cc27c123e416dd8e
uae <aniekaudo@yahoo.com>
2022-11-09 16:23:39 +0100

    :art: Footer
    Updatye footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -55,7 +55,7 @@ const Footer = () => {
         style={{ backgroundColor: '#2b2b2b', borderTop: 'solid 1px #707070' }}
       >
         <p className={styles.bottom}>
-          Copyright© {Foter()} 
+          Copyright© {Foter()} &nbsp;
           All right reserved{' '}
         </p>{' '}
       </div>{' '}
